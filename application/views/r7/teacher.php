bd10a3f3eb9e08f8b2b419feacb4193c42b51dbe
Shiquan Sun <me@imsun.net>
2015-07-09 20:40:47 +0800

    fix path bugs

--- application/views/r7/teacher.php
+++ application/views/r7/teacher.php
@@ -1,5 +1,5 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-            <iframe style="position: absolute; left: 0; top: 0; width: 100%; height: 100%; border: 0" src="<?php echo site_url('r7_static/teacher.html');?>"></iframe>
+            <iframe style="position: absolute; left: 0; top: 0; width: 100%; height: 100vh; border: 0" src="<?php echo site_url('/r7_static/teacher.html');?>"></iframe>
         </div>
 
     </div>
