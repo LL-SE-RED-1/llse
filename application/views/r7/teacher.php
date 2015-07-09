a5b1035ae2055178e7a6d2350d2f2a5d40cc678f
Shiquan Sun <me@imsun.net>
2015-07-09 21:18:30 +0800

    bug fixed

--- application/views/r7/teacher.php
+++ application/views/r7/teacher.php
@@ -1,5 +1,5 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-            <iframe style="position: absolute; left: 0; top: 0; width: 100%; height: 100vh; border: 0" src="<?php echo site_url('../r7_static/'.$path.'html');?>"></iframe>
+            <iframe style="position: absolute; left: 0; top: 0; width: 100%; height: 100vh; border: 0" src="<?php echo site_url('../r7_static/'.$path.'.html');?>"></iframe>
         </div>
 
     </div>
